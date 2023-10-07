c2301b38d653cf9f099c71131a34ebaef53f1d39
Ian Kimani <ianmwng@gmail.com>
2023-10-07 09:00:08 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
+  Iterate from the beggining to the middle 
+  Check each leter to the corresponding letter from the end 
+  if any letters dont match return false
 */
 
 /*
@@ -15,6 +25,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
